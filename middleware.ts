2b3a42642d6f5a6ae32a634c8b46d65cef634970
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2025-03-22 22:56:54 +0200

    fix middleware 2

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,21 @@
-import { NextResponse } from "next/server"
-import { getToken } from "next-auth/jwt"
-import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
-  console.log("Extracted Token:", token);
-  // Check if the path starts with /admin
-  if (request.nextUrl.pathname.startsWith("/admin")) {
-    console.log("Admin path accessed")
-    // If not logged in or not an admin, redirect to login
-    if (!token || !token.role || token.role !== "admin") {
-      console.log("Unauthorized access or missing role:", token);
-      const url = new URL("/login", request.url);
-      url.searchParams.set("callbackUrl", request.url);
-      return NextResponse.redirect(url);
+export default async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+    if (request.nextUrl.pathname.startsWith("/admin")) {
+        if (!token || token.role !== "admin") {
+            const url = new URL("/login", request.url);
+            url.searchParams.set("callbackUrl", request.url);
+            return NextResponse.redirect(url);
+        }
     }
 
-  }
-
-  // Check if the path is for authenticated users only
-  if (
-    request.nextUrl.pathname.startsWith("/profile") ||
-    request.nextUrl.pathname.startsWith("/watchlist") ||
-    request.nextUrl.pathname.startsWith("/settings") ||
-    request.nextUrl.pathname.startsWith("/notifications")
-  ) {
-    // If not logged in, redirect to login
-    if (!token) {
-      const url = new URL("/login", request.url)
-      url.searchParams.set("callbackUrl", request.url)
-      return NextResponse.redirect(url)
-    }
-  }
-
-  return NextResponse.next()
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/profile/:path*", "/watchlist/:path*", "/settings/:path*", "/notifications/:path*"],
-}
-
+    matcher: ["/admin/:path*"],
+};
